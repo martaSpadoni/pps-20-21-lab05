19794c25a12b7994a1d2af638aefeca1edde70a1
Marta Spadoni <marta.spadoni2@studio.unibo.it>
2021-04-05 16:28:02 +0200

    added exceptions

--- src/u05lab/code/Ex2-ExamsManager.scala
+++ src/u05lab/code/Ex2-ExamsManager.scala
@@ -5,8 +5,12 @@ import u05lab.code.Kind._
 
 trait Kind
 object Kind {
-  case class RETIRED() extends Kind
-  case class FAILED() extends Kind
+  case class RETIRED() extends Kind {
+    override def toString: String = "RETIRED"
+  }
+  case class FAILED() extends Kind{
+    override def toString: String = "FAILED"
+  }
   case class SUCCEEDED() extends Kind
 }
 
@@ -22,19 +26,16 @@ trait ExamResult{
   }
 }
 
-class ExamResultImpl (val kind: Kind, val evaluation:Option[Int]=Option.empty, val cumLaude:Boolean=false) extends ExamResult
+private class ExamResultImpl (val kind: Kind, val evaluation:Option[Int], val cumLaude:Boolean) extends ExamResult
 
 object ExamResult{
 
   def apply(kind: Kind, eval: Option[Int] = Option.empty,
             cumLaude:Boolean = false): ExamResult = new ExamResultImpl(kind, eval, cumLaude)
-
   def failed: ExamResult = ExamResult(FAILED())
   def retired: ExamResult = ExamResult(RETIRED())
   def succeeded(evaluation: Int): ExamResult = ExamResult(SUCCEEDED(), Some(evaluation))
   def succeededCumLaude : ExamResult = ExamResult(SUCCEEDED(), Some(30), true)
-
-
 }
 
 sealed trait ExamsManager {
@@ -50,21 +51,32 @@ class ExamsManagerImpl() extends ExamsManager {
 
   private var map : Map[String, Map[String, ExamResult]] = Map()
 
-  override def createNewCall(call: String): Unit = map = map + (call -> Map())
+  override def createNewCall(call: String): Unit = {
+    if (map.contains(call)) throw new IllegalArgumentException
+    map = map + (call -> Map())
+  }
 
-  override def addStudentResult(call: String, student: String, result: ExamResult): Unit =
-    map = map + (call -> (map(call) + (student -> result)))
+  override def addStudentResult(call: String, student: String, result: ExamResult): Unit = {
+    val m = map.getOrElse(call, throw new IllegalArgumentException)
+    if (m.contains(student)) throw new IllegalArgumentException
+    map = map + (call -> (m + (student -> result)))
+  }
 
   override def getAllStudentsFromCall(call: String): Set[String] = map(call).keySet
 
-  override def getEvaluationsMapFromCall(call: String): Map[String, Int] =
-    map(call).filter((m) => m._2.evaluation.isDefined).mapValues(r => r.evaluation.get)
+  override def getEvaluationsMapFromCall(call: String): Map[String, Int] = {
+    val m = map.getOrElse(call, throw new IllegalArgumentException)
+    m.collect({case(key, res) if res.evaluation.isDefined => key -> res.evaluation.get})
+  }
 
   override def getResultsMapFromStudent(student: String): Map[String, String] =
-    map.filter((m) => m._2.isDefinedAt(student)).map{case (c, rm) => (c, rm(student).toString)}
+    map.collect({case(call, results) if results.isDefinedAt(student) &&
+      results(student).evaluation.isDefined => call -> results(student).evaluation.get.toString})
 
-  override def getBestResultFromStudent(student: String): Option[Int] =
-    map.values.flatMap(m => m.filterKeys(s => s == student)).map{case (s, r) => r.evaluation}.max
+  override def getBestResultFromStudent(student: String): Option[Int] = {
+    val results = map.values.flatMap(m => m.filterKeys(s => s == student)).map{case (_, r) => r.evaluation}
+    if(!results.isEmpty) results.max else None
+  }
 }
 
 object ExamsManager{
@@ -73,15 +85,15 @@ object ExamsManager{
 
 object ExamsManagerTest extends App {
 
-  println(ExamResult.failed.kind) //FAILED()
+  println(ExamResult.failed.kind) //FAILED
   println(ExamResult.failed.evaluation.isDefined) //false
   println(ExamResult.failed.cumLaude) //false
-  println(ExamResult.failed.toString) //FAILED()
+  println(ExamResult.failed.toString) //FAILED
   println()
-  println(ExamResult.retired.kind) //RETIRED()
+  println(ExamResult.retired.kind) //RETIRED
   println(ExamResult.retired.evaluation.isDefined) //false
   println(ExamResult.retired.cumLaude) //false
-  println(ExamResult.retired.toString) //RETIRED()
+  println(ExamResult.retired.toString) //RETIRED
   println()
   println(ExamResult.succeeded(28).kind) //SUCCEEDED()
   println(ExamResult.succeeded(28).evaluation) //Some(28)
@@ -115,7 +127,7 @@ object ExamsManagerTest extends App {
   println(em.getAllStudentsFromCall("gennaio"))
   println(em.getAllStudentsFromCall("marzo"))
 
-  println(em.getEvaluationsMapFromCall("gennaio"))
+  println("evaluation map from call" + em.getEvaluationsMapFromCall("gennaio"))
 
   println(em.getResultsMapFromStudent("rossi"))
   println(em.getResultsMapFromStudent("bianchi"))
@@ -125,5 +137,6 @@ object ExamsManagerTest extends App {
   println(em.getBestResultFromStudent("bianchi"))
   println(em.getBestResultFromStudent("neri"))
   println(em.getBestResultFromStudent("viola"))
+  println(em.getBestResultFromStudent("marta"))
 
 }
